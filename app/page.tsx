8f022c079e525db2732210f88d4ef93da3adb5b9
muradimusa014-hub <muradimusa014@gmail.com>
2026-07-02 17:07:11 +0100

    Update background image source in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         {/* The Background */}
         <div className="absolute inset-0 z-0 bg-slate-950">
           <img 
-            src="https://images.unsplash.com/photo-1503387762-592deb58ef4e?q=80&w=2000&auto=format&fit=crop" 
+            src="https://images.unsplash.com/photo-1485965373059-f07657e9f841?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="Engineering Site" 
             className="w-full h-full object-cover opacity-30"
           />
